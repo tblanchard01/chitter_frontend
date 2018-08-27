4524369223926787a157720d2f5837dd409d3d54
Chris <chrisblanchard@gmx.co.uk>
2018-08-27 16:30:47 +0100

    added responsive peep box

--- src/components/PostPeep.js
+++ src/components/PostPeep.js
@@ -1,15 +1,22 @@
 import React from "react";
 class PostPeep extends React.Component {
+  state = {}
   render() {
-    return (
-      <div className="post_peep_box">
-        <h2> Welcome User </h2>
-        <form action="/user_peep" method="post">
-          <textarea name="user_peep" className="peepinput" />
-          <input type="submit" value="Peep" />
-        </form>
-      </div>
-    );
+    if (this.props.username) {
+      return (
+        <div className="post_peep_box">
+          <h2> Welcome {this.props.username} </h2>
+          <form action="/user_peep" method="post">
+            <textarea name="user_peep" className="peepinput" />
+            <input type="submit" value="Peep" />
+          </form>
+        </div>
+
+      );
+    }
+
+    return <div className="post_peep_box"><h2>Please create username to peep!</h2></div>
+
   }
 }
 
